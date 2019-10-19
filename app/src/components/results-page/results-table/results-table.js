38adf7795aac5055a6ce63b166e27aae0af228b2
Jeremy Scott <jeremyrobertsteele@gmail.com>
2019-10-19 13:37:53 +1100

    Remove Titles

--- app/src/components/results-page/results-table/results-table.js
+++ app/src/components/results-page/results-table/results-table.js
@@ -20,17 +20,6 @@ className ResultsTable extends React.Component {
     return (
       <section className="results-table">
         <h2>Title</h2>
-        <div className="results-header-container">
-          <header className="results-header-item" id="results-header-score">
-              <h3>Sentiment Score</h3>
-          </header>
-          <header className="results-header-item" id="results-header-stars">
-              <h3></h3>
-          </header>
-          <header className="results-header-item" id="results-header-title">
-              <h3>Article Title</h3>
-          </header>
-        </div>
         <section className="results-data-container">
           <ResultsCard />
         </section>
